725c0f34830ffe0c72e297d855bbb8c9c14eac71
Indah Purnam Sari <56105225+indah-purnama@users.noreply.github.com>
2019-10-26 11:08:28 -0700

    Upload tugas kuis 01

---
+++ 2120180158-K01-PBO-TIA20192020-3-4-02/src/biodata/Biodata.java
@@ -0,0 +1,36 @@
+package biodata;
+public class Biodata {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        mahasiswa mahasiswa_1=new mahasiswa();
+        mahasiswa_1.alamat ="DS.kunci";
+        mahasiswa_1.nama="Indah purnama";
+        mahasiswa_1.nim="2120180158";
+        mahasiswa_1.biodata();
+        
+        mahasiswa mahasiswa_2=new mahasiswa();
+        mahasiswa_2.alamat ="Ds.sumberarum";
+        mahasiswa_2.nama="Galih parwati";
+        mahasiswa_2.nim="2120180151";
+        mahasiswa_2.biodata();
+        
+        mahasiswa mahasiswa_3=new mahasiswa();
+        mahasiswa_3.alamat ="Ds.Ngunut";
+        mahasiswa_3.nama="Lia agustina";
+        mahasiswa_3.nim="2120180165";
+        mahasiswa_3.biodata();
+        
+        mahasiswa mahasiswa_4=new mahasiswa();
+        mahasiswa_4.alamat ="Ds.Baureno";
+        mahasiswa_4.nama="Indika Fahrul";
+        mahasiswa_4.nim="2120180159";
+        mahasiswa_4.biodata();
+        
+        mahasiswa mahasiswa_5=new mahasiswa();
+        mahasiswa_5.alamat ="Ds.Ngraho";
+        mahasiswa_5.nama="Adi Nella";
+        mahasiswa_5.nim="2120180130";
+        mahasiswa_5.biodata();
+    }
+    
+}
---
+++ 2120180158-K01-PBO-TIA20192020-3-4-02/src/biodata/mahasiswa.java
@@ -0,0 +1,13 @@
+package biodata;
+public class mahasiswa {
+    public String alamat;
+    public String nama;
+    public String nim;
+    
+    public void biodata(){
+        System.out.println("alamat"+alamat);
+        System.out.println("nama"+nama);
+        System.out.println("nim"+nim);
+    
+}
+}
